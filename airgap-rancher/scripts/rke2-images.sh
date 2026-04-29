162fc3ea7bb90b093756a1d7527de4f6f00768d0
Adam Acosta <adamacosta@users.noreply.github.com>
2026-04-29 10:46:26 -0500

    push to other

--- airgap-rancher/scripts/rke2-images.sh
+++ airgap-rancher/scripts/rke2-images.sh
@@ -4,13 +4,15 @@ set -euo pipefail
 
 WORKDIR=$(mktemp -d)
 RKE2_VERSION="$1"
+RKE2_TAG=$(echo $RKE2_VERSION | tr '+' '-')
 
 pushd "${WORKDIR}"
 
 curl -fsLSO "https://github.com/rancher/rke2/releases/download/${RKE2_VERSION}/rke2-images-core.linux-amd64.txt"
 curl -fsLSO "https://github.com/rancher/rke2/releases/download/${RKE2_VERSION}/rke2-images-cilium.linux-amd64.txt"
+curl -fsLSO "https://github.com/rancher/rke2/releases/download/${RKE2_VERSION}/rke2-images-traefik.linux-amd64.txt"
 
-cat rke2-images-*.txt | sed -E '/aws|azure|nginx/d' | sed 's/docker\.io/rgcrprod\.azurecr\.us/g' \
+cat rke2-images-*.txt | sed -E '/aws|azure|nginx/d' | sed 's/docker\.io/registry\.ranchercarbide\.dev/g' \
   > rke2-images.txt
 
 popd
@@ -27,5 +29,8 @@ EOF
 while read -r img; do
   echo "    - name: ${img}"
 done < "${WORKDIR}/rke2-images.txt" >> rke2-manifest.yaml
+
+# Add system-agent-installer needed for Rancher provisioning
+echo "    - name: registry.ranchercarbide.dev/rancher/system-agent-installer-rke2:${RKE2_TAG}" >> rke2-manifest.yaml
 
 rm -r "${WORKDIR}"
